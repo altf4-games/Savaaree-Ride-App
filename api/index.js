c4089840721b7395a21a4e40ab72b8dcaf415033
Pradyum Mistry <39883432+AltF4-Games@users.noreply.github.com>
2024-04-13 14:20:07 +0530

    Fixed Map

--- api/index.js
+++ api/index.js
@@ -4,11 +4,7 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public", "index.html"));
-});
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
